3405fc422ce1330dfa6fe1c0c203825fca8db7c3
boytrend0108 <boytrend@gmail.com>
2025-09-29 09:29:22 +0300

    feat: start gh auth flow

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { auth, signIn, signOut } from '@/auth';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  className?: string;
+}
+
+export const Navbar: React.FC<Props> = async (props) => {
+  const { className, ...otherProps } = props;
+  const session = await auth();
+
+  return (
+    <header className={`p-4 bg-white  ${className ?? ''}`} {...otherProps}>
+      <nav className='flex justify-between items-center'>
+        <Link href='/'>
+          <Image src='/logo.png' alt='Logo' width={150} height={60} />
+        </Link>
+
+        <div className='flex justify-between items-center gap-4 text-black'>
+          {session && session?.user ? (
+            <>
+              <Link href='/startup/create'>Create</Link>
+
+              <button onClick={() => signOut()}>
+                <Link href='/startup/create'>Create</Link>
+
+                <span>Sign Out</span>
+              </button>
+
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              className='cursor-pointer'
+              action={async () => {
+                'use server';
+
+                await signIn('github');
+              }}
+            >
+              <button type='submit'>Sign In</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
